a3893f98ff99c6b572be88966a31d0e9f25eb1c0
minorDeveloper <theminordeveloper@outlook.com>
2016-10-22 20:14:02 +0100

    First Commit - Visual Studio Project and Batch Files

---
+++ Source/MKS_Kontracts/MKSKontracts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MKS_Kontracts
+{
+    public class MKSKontracts
+    {
+        string cheeeedesey = "";
+    }
+}
